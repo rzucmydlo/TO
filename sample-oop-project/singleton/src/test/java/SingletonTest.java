5f1bbdae22b2245aef5d943d87a94db951eabdc6
Student Domena <dstudent@ii.local>
2019-11-27 18:32:41 +0100

    first commit

---
+++ sample-oop-project/singleton/src/test/java/SingletonTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class SingletonTest {
+
+    @Test
+    public void shouldPass() {
+        assertNotEquals(2, 3);
+    }
+}
